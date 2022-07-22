f52321aa9c54ce73a83174923bd7a22e4bdcc1c1
Aleksandr_Mulgachev <Aleksandr_Mulgachev@epam.com>
2022-07-22 22:59:19 +0300

    Task4.

---
+++ src/task4/tree.test.js
@@ -0,0 +1,75 @@
+import { tree } from "./tree.js";
+import fs from 'fs/promises';
+import path from 'path';
+
+jest.mock('fs/promises');
+jest.mock('path');
+
+describe('tree', () => {
+    const pathInput = 'some-path';
+    let actual;
+
+    beforeAll(() => {
+        path.resolve.mockImplementation((x, y) => `${x}\\${y}`);
+    });
+
+    describe('empty folder', () => {
+        beforeEach(async () => {
+            //act
+            actual = await tree('');
+        });
+
+        it('should return empty arrays', () => {
+            expect(actual.files).toEqual([]);
+            expect(actual.folders).toEqual([]);
+        });
+    });
+
+    describe('path only with files', () => {
+        const expectedFiles = ['file1','file2','file3'];
+        beforeEach(async () => {
+            //arrange
+            fs.readdir.mockImplementationOnce(() => Promise.resolve(expectedFiles));
+            fs.stat.mockImplementationOnce(() => Promise.resolve({
+                    isDirectory: () => false
+                }));
+
+            //act
+            actual = await tree(pathInput);
+        });
+
+        it('should return empty arrays', () => {
+            expect(actual.files).toEqual(expectedFiles.map(file => `${pathInput}\\${file}`));
+            expect(actual.folders).toEqual([]);
+        });
+    });
+
+    describe('path folder with files', () => {
+        const subFolderName = 'subFolder';
+        const rootDirItems = ['file1','file2', subFolderName];
+        const subDirItems = ['file3','file4'];
+        beforeEach(async () => {
+            //arrange
+            fs.readdir.mockImplementationOnce(() => Promise.resolve(rootDirItems));
+            fs.readdir.mockImplementationOnce(() => Promise.resolve(subDirItems));
+            fs.stat.mockImplementation((item) => Promise.resolve({
+                isDirectory: () => {
+                    return item.endsWith(subFolderName)
+                }
+            }));
+
+            //act
+            actual = await tree(pathInput);
+        });
+
+        it('should return empty arrays', () => {
+            expect(actual.files).toEqual([
+                `${pathInput}\\${rootDirItems[0]}`,
+                `${pathInput}\\${rootDirItems[1]}`,
+                `${pathInput}\\${rootDirItems[2]}\\${subDirItems[0]}`,
+                `${pathInput}\\${rootDirItems[2]}\\${subDirItems[1]}`
+            ]);
+            expect(actual.folders).toEqual([`${pathInput}\\${rootDirItems[2]}`]);
+        });
+    });
+});
